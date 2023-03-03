6b8de38876b81beb048d2619f3176f5d3c7d1831
A-poc <100603074+A-poc@users.noreply.github.com>
2023-03-03 14:15:19 +0000

    Update certSniff.py
    Changed banner message, added ANS code highlighting.

--- certSniff.py
+++ certSniff.py
@@ -4,45 +4,48 @@ import datetime
 import certstream
 import argparse
 import time
-
-print("""
-                              █████     █████████              ███     ██████     ██████ 
-                             ░░███     ███░░░░░███            ░░░     ███░░███   ███░░███
-  ██████   ██████  ████████  ███████  ░███    ░░░  ████████   ████   ░███ ░░░   ░███ ░░░ 
- ███░░███ ███░░███░░███░░███░░░███░   ░░█████████ ░░███░░███ ░░███  ███████    ███████   
-░███ ░░░ ░███████  ░███ ░░░   ░███     ░░░░░░░░███ ░███ ░███  ░███ ░░░███░    ░░░███░    
-░███  ███░███░░░   ░███       ░███ ███ ███    ░███ ░███ ░███  ░███   ░███       ░███     
-░░██████ ░░██████  █████      ░░█████ ░░█████████  ████ █████ █████  █████      █████    
- ░░░░░░   ░░░░░░  ░░░░░        ░░░░░   ░░░░░░░░░  ░░░░ ░░░░░ ░░░░░  ░░░░░      ░░░░░     
-Certificate Transparency Log Sniffer
------------------------------------------------------------------------------------------
-    """)
+FAIL        = '\033[91m'
+ENDC        = '\033[0m'
+OKGREEN     = '\033[92m'
+BOLD        = '\033[1m'
+OKBLUE      = '\033[94m'
+WARNING     = '\033[93m'
+GREY        = '\033[90m'
+PURPLE      = '\033[35m'
+DARKPURPLE  = '\033[34m'
+def banner():
+    print(PURPLE+"""
+╔═╗┌─┐┬─┐┌┬┐╔═╗┌┐┌┬┌─┐┌─┐
+║  ├┤ ├┬┘ │ ╚═╗││││├┤ ├┤ 
+╚═╝└─┘┴└─ ┴ ╚═╝┘└┘┴└  └  
+Certificate Transparency Log Sniffer"""+GREY+"""
+-----------------------------------------------------------------------------------------""")
 
 parser=argparse.ArgumentParser(description='Domain keyword sniffer using certfiicate transparency logs.')
 parser.add_argument('-f','--file',help='file containing keywords to sniff (default: monitor.txt).',required=False)
 parser.add_argument('-v','--verbose',help='verbose output (All domains passing through).',required=False,action='store_true')
 args=vars(parser.parse_args())
-
+banner()
 if args['file'] is not None:
     filename=args['file']
     try:
         with open(str(filename), 'r') as f:
             triggerWords=[line.strip() for line in f]
-            print("Using sniff words from ["+str(filename)+"]")
+            print(GREY+"Using sniff words from ["+str(filename)+"]"+ENDC)
             print("")
             time.sleep(2)
     except:
-        print("Could not read file ["+str(filename)+"]")
+        print(WARNING+"Could not read file ["+str(filename)+"]"+ENDC)
         exit()
 else:
     try:
         with open('monitor.txt', 'r') as f:
             triggerWords=[line.strip() for line in f]
-            print("Using sniff words from [monitor.txt]")
+            print(GREY+"Using sniff words from [monitor.txt]"+ENDC)
             print("")
             time.sleep(2)
     except Exception as error:
-        print("Could not open sniff word file ("+str(error)+")")
+        print(WARNING+"Could not open sniff word file ("+str(error)+")"+ENDC)
         exit()
 
 def print_callback(message, context):
@@ -55,11 +58,11 @@ def print_callback(message, context):
         else:
             domain=all_domains[0]
             if args['verbose'] is True:
-                print(str(datetime.datetime.now().strftime('%m/%d/%y %H:%M:%S')+" - "+domain)) 
+                print(BOLD+PURPLE+"["+ENDC+GREY+str(datetime.datetime.now().strftime('%m/%d/%y %H:%M:%S')+BOLD+PURPLE+"]"+DARKPURPLE+":"+PURPLE+"["+ENDC+GREY+(domain[2:] if domain.startswith('*') else domain)+BOLD+PURPLE+"]"))
             if any(trigger in domain for trigger in triggerWords):
-                print(str(datetime.datetime.now().strftime('%m/%d/%y %H:%M:%S')+" + "+domain))
+                print(BOLD+PURPLE+"["+ENDC+GREY+str(datetime.datetime.now().strftime('%m/%d/%y %H:%M:%S')+BOLD+PURPLE+"]"+DARKPURPLE+":"+PURPLE+"["+ENDC+GREY+(domain[2:] if domain.startswith('*') else domain)+BOLD+PURPLE+"]"))
                 with open('log.txt', 'a') as f:
-                    f.write(str(datetime.datetime.now().strftime('%m/%d/%y %H:%M:%S')+" - "+domain)+"\n")
+                    f.write(str(domain[2:] if domain.startswith('*') else domain)+"\n")
         sys.stdout.flush()
 
 certstream.listen_for_events(print_callback, url='wss://certstream.calidog.io/')
